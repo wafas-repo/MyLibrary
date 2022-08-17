ae78831418839b0643e550bb532cce9da9328e26
Wafa Qazi <wafa.qazi@outlook.com>
2022-08-17 00:49:39 -0400

    added form to create new book card

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 let myLibrary = [];
 
-function Book(title, author, num_pages) {
+function Book(title, author, num_pages, read) {
 
     this.title = title
     this.author = author
     this.num_pages = num_pages
+    this.read = read
 
 }
 
 function addBookToLibrary() {
-   title =  'Harry Potter and the Goblet of fire'
-   author = 'J.K.Rowling'
-   num_pages = 264
+   
+    let form= document.querySelector("form");                        
+    form.addEventListener('submit',function (e){                     
+        let title = document.getElementById("title").value;     
+        let author = document.getElementById("author").value;
+        let pages = document.getElementById("pages").value;
+        let read =  document.getElementById("readStatus").checked;
+        let newBook = new Book(title, author, pages, read);
+        myLibrary.push(newBook)              
+        e.preventDefault();
+        form.reset();  
+        createCard(newBook);                                 
+    });
 
-   title2 =  'Harry Potter and the prisoner of Azkaban'
-   author2 = 'J.K.Rowling'
-   num_pages2 = 366
-
-   const book = new Book(title, author, num_pages)
-   const newBook = new Book(title2, author2, num_pages2)
-   myLibrary.push(book)
-   myLibrary.push(newBook)
-
-}
-
-function displayBooks() {
-
-    for (let i = 0; i < myLibrary.length; i++) {
-        createCard(myLibrary[i]);
-    }
-    
 }
 
 function createCard(book) {
@@ -40,18 +34,31 @@ function createCard(book) {
     let author = document.createElement("p");
     let pageNum = document.createElement("p");
     let card_info = document.createElement("div");
+    let buttons = document.createElement("div");
+    let readBtn = document.createElement("button");
+    let removeBtn = document.createElement("button");
 
     newBookCard.classList.add('card')
 
     title.innerHTML = book.title;
     author.innerHTML = book.author;
     pageNum.innerHTML = book.num_pages;
+    removeBtn.textContent = 'Remove'
+    
+    if (book.read) {
+        readBtn.textContent = 'Read'
+    } else {
+        readBtn.textContent = 'Not Read'
+    }
 
     card_info.appendChild(title);
     card_info.appendChild(author);
     card_info.appendChild(pageNum);
+    buttons.appendChild(readBtn);
+    buttons.appendChild(removeBtn);
 
     newBookCard.appendChild(card_info);
+    newBookCard.appendChild(buttons);
 
     shelf.appendChild(newBookCard)
 
@@ -62,6 +69,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
     addBookToLibrary();
     console.log(myLibrary)
-    displayBooks()
     
 });
