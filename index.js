66f2bbd24b77f131a51cb6f2118772d98522a745
Wafa Qazi <wafa.qazi@outlook.com>
2022-08-16 23:38:29 -0400

    styled book cards

--- index.js
+++ index.js
@@ -1,22 +1,67 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, num_pages) {
+
+    this.title = title
+    this.author = author
+    this.num_pages = num_pages
+
 }
 
 function addBookToLibrary() {
-  // do stuff here
+   title =  'Harry Potter and the Goblet of fire'
+   author = 'J.K.Rowling'
+   num_pages = 264
+
+   title2 =  'Harry Potter and the prisoner of Azkaban'
+   author2 = 'J.K.Rowling'
+   num_pages2 = 366
+
+   const book = new Book(title, author, num_pages)
+   const newBook = new Book(title2, author2, num_pages2)
+   myLibrary.push(book)
+   myLibrary.push(newBook)
+
+}
+
+function displayBooks() {
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        createCard(myLibrary[i]);
+    }
+    
+}
+
+function createCard(book) {
+
+    const shelf = document.getElementById('shelf');
+    const newBookCard = document.createElement('div');
+    let title = document.createElement("p");                  
+    let author = document.createElement("p");
+    let pageNum = document.createElement("p");
+    let card_info = document.createElement("div");
+
+    newBookCard.classList.add('card')
+
+    title.innerHTML = book.title;
+    author.innerHTML = book.author;
+    pageNum.innerHTML = book.num_pages;
+
+    card_info.appendChild(title);
+    card_info.appendChild(author);
+    card_info.appendChild(pageNum);
+
+    newBookCard.appendChild(card_info);
+
+    shelf.appendChild(newBookCard)
+
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    let dummy_book = {
-        'title': 'Harry Potter and the Goblet of Fire',
-        'author': 'J.K.Rowling',
-        'number_of_pages': 264,
-    }
-
-    myLibrary.push(dummy_book)
+    addBookToLibrary();
     console.log(myLibrary)
+    displayBooks()
     
 });
